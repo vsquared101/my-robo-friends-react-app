7370b48d44d69cfb5c8a3afeb9a13457648d6786
Vivek Varma <vsquared101@gmail.com>
2020-05-14 13:50:17 +0530

    Organize code structure.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
-import CardList from './CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import CardList from '../components/CardList';
 
 class App extends Component {
   constructor(){
