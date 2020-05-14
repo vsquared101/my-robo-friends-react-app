262e8f13087677ed9cc933a0313eaa940c4987c1
Vivek Varma <vsquared101@gmail.com>
2020-05-14 16:04:43 +0530

    Updates

---
+++ precache-manifest.6cf809a66c9b10ad9168cccab96f4243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2401e9573e09e81663b01bf360eb4b0",
+    "url": "/my-robo-friends-react-app/index.html"
+  },
+  {
+    "revision": "2a64f0e443d636e19004",
+    "url": "/my-robo-friends-react-app/static/css/main.a93061a0.chunk.css"
+  },
+  {
+    "revision": "b8c0082425e88c98925c",
+    "url": "/my-robo-friends-react-app/static/js/2.d00b6b54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-robo-friends-react-app/static/js/2.d00b6b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a64f0e443d636e19004",
+    "url": "/my-robo-friends-react-app/static/js/main.28348122.chunk.js"
+  },
+  {
+    "revision": "498f9df7b5e9eb2cb4d7",
+    "url": "/my-robo-friends-react-app/static/js/runtime-main.800036df.js"
+  },
+  {
+    "revision": "d76e3f1e7618306c25bca07f875c6236",
+    "url": "/my-robo-friends-react-app/static/media/PencilLetters.d76e3f1e.woff"
+  }
+]);
